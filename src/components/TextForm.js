54d4f93274fce9f024465235ded4ea6949365c07
Harish Pandey <pandeyharish394@gmail.com>
2023-11-18 17:17:52 +0545

    made the selected btn outline

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,70 +10,100 @@ export default function TextForm(props) {
   const [isCenter, centered] = useState(false);
   const [isLeft, leftAligned] = useState(false);
   const [isJustified, justified] = useState(false);
-const refreshValue = (e) => {
-  updatedText(e.target.value);
-};
-
-
-
+  const refreshValue = (e) => {
+    updatedText(e.target.value);
+  };
 
   const convertToUpperCase = () => {
     let newText = text.toUpperCase();
     updatedText(newText);
   };
-  
+
   const bold = () => {
     setIsBold((prevState) => !prevState);
   };
   const italic = () => {
-    makeItalic((prevState) => !prevState); 
+    makeItalic((prevState) => !prevState);
   };
   const underline = () => {
-    underlined((prevState)=> !prevState);
+    underlined((prevState) => !prevState);
   };
   const centerAlign = () => {
-    centered((prevState) => !prevState);  
+    centered((prevState) => !prevState);
   };
   const rightAlign = () => {
     rightAligned((prevState) => !prevState);
   };
   const leftAlign = () => {
     leftAligned((prevState) => !prevState);
-  };  
+  };
   const justify = () => {
-    justified((prevState)=> !prevState);
+    justified((prevState) => !prevState);
   };
   return (
     <>
       <div className="container">
         <h1>{props.heading}</h1>
         <div>
-          <button className="btn btn-light" onClick={bold}>
-            {isBold ? "B X" : <i class="ri-bold fontbold"></i>}
+          <button
+            className={`btn btn-light ${isBold ? "btn-outline-primary" : ""}`}
+            onClick={bold}
+          >
+            <i class="ri-bold fontbold"></i>
           </button>
 
-          <button className="btn btn-light m-1" onClick={italic}>
-            {isItalic ? "I" : <i class="ri-italic fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isItalic ? "btn-outline-primary" : ""
+            }`}
+            onClick={italic}
+          >
+            <i class="ri-italic fontbold"></i>
           </button>
 
-          <button className="btn btn-light m-1" onClick={underline}>
-            {isUnderline ? "U" : <i class="ri-underline fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isUnderline ? "btn-outline-primary" : ""
+            }`}
+            onClick={underline}
+          >
+            <i class="ri-underline fontbold"></i>
           </button>
 
-          <button className="btn btn-light m-1" onClick={leftAlign}>
-            {isLeft ? "L X" : <i class="ri-align-left fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isLeft ? "btn-outline-primary" : ""
+            }`}
+            onClick={leftAlign}
+          >
+            <i class="ri-align-left fontbold"></i>
           </button>
 
-          <button className="btn btn-light m-1" onClick={centerAlign}>
-            {isCenter ? "X" : <i class="ri-align-center fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isCenter ? "btn-outline-primary" : ""
+            }`}
+            onClick={centerAlign}
+          >
+            <i class="ri-align-center fontbold"></i>
           </button>
 
-          <button className="btn btn-light m-1" onClick={rightAlign}>
-            {isRight ? "R" : <i class="ri-align-right fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isRight ? "btn-outline-primary" : ""
+            }`}
+            onClick={rightAlign}
+          >
+            <i class="ri-align-right fontbold"></i>
           </button>
 
-          <button className="btn btn-light" onClick={justify}>
-            {isJustified ? "J" : <i class="ri-align-justify fontbold"></i>}
+          <button
+            className={`btn btn-light m-1 ${
+              isJustified ? "btn-outline-primary" : ""
+            }`}
+            onClick={justify}
+          >
+            <i class="ri-align-justify fontbold"></i>
           </button>
         </div>
         <div className="input-group mt-4">
@@ -95,7 +125,7 @@ const refreshValue = (e) => {
                 ? "right"
                 : isJustified
                 ? "justify"
-                : "normal",
+                : "left",
             }}
           ></textarea>
         </div>
@@ -113,8 +143,3 @@ const refreshValue = (e) => {
     </>
   );
 }
-
-
-
-
-
